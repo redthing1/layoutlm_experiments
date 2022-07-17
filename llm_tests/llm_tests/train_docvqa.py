ea3383e694b7c24c301ebdb6d933e4ecccf6293d
redthing1 <redthing1@alt.icu>
2022-07-16 19:01:23 -0700

    Revert "divide steps by batch ?"
    This reverts commit fa3f44cc842e84336bd14c1e323815d766592ac6.

--- llm_tests/llm_tests/train_docvqa.py
+++ llm_tests/llm_tests/train_docvqa.py
@@ -64,7 +64,7 @@ def cli(
     training_args = TrainingArguments(
         output_dir="./train_output",
         overwrite_output_dir=True,
-        max_steps=steps // batch,
+        max_steps=steps,
         learning_rate=lr,
         # warmup_steps=int(steps * warmup_ratio),
         warmup_ratio=warmup_ratio,
