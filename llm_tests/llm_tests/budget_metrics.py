f08dcf6357f51db2b13486ea74cae1d978274df4
redthing1 <redthing1@alt.icu>
2022-07-21 16:27:48 -0700

    add an anls metric

--- llm_tests/llm_tests/budget_metrics.py
+++ llm_tests/llm_tests/budget_metrics.py
@@ -20,12 +20,15 @@ from transformers import (
 )
 from datasets import load_dataset, load_from_disk
 
-def fuzzy(s1, s2, threshold=0.2):
-    return (editdistance.eval(s1, s2) / ((len(s1) + len(s2)) / 2)) < threshold
+def normalized_levenshtein(s1, s2):
+    L = editdistance.eval(s1, s2)
+    norm_div = max(len(s1), len(s2))
+    return L / norm_div
 
 def cli(
     model_path: str,
     val_data_path: str,
+    tiny_subset: bool = False,
 ):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -40,9 +43,17 @@ def cli(
     val_data = load_from_disk(val_data_path)
     print(f"val data loaded: {val_data_path}")
 
-    val_data.set_format("torch")
+    if tiny_subset:
+        val_data = val_data.select(range(20))
 
-    dataloader = torch.utils.data.DataLoader(val_data, batch_size=1)
+    val_data_encoded = val_data.map(lambda x: x)
+    val_data_encoded = val_data_encoded.remove_columns(["acceptable_answers", "acceptable_answers_starts", "acceptable_answers_ends"])
+    val_data_encoded.set_format("torch")
+
+    print('encoded val data:', val_data_encoded)
+    print('encoded val data:', val_data_encoded.features)
+
+    dataloader = torch.utils.data.DataLoader(val_data_encoded, batch_size=1)
 
     def check_preds(outputs, labels):
         answer_start_scores = outputs.start_logits.cpu().detach()
@@ -77,27 +88,41 @@ def cli(
         #     f"answer probs: total {answer_probs}, start {answer_start_prob_max}, end {answer_end_prob_max}"
         # )
 
-        expected_start_pos = labels["start_positions"][0]
-        expected_end_pos = labels["end_positions"][0] + 1
-        expected_answer = tokenizer.decode(
-            input_id_list[expected_start_pos:expected_end_pos]
-        )
+        # expected_start_pos = labels["start_positions"][0]
+        # expected_end_pos = labels["end_positions"][0] + 1
+        # expected_answer = tokenizer.decode(
+        #     input_id_list[expected_start_pos:expected_end_pos]
+        # )
         # print(
         #     f"expected answer: {expected_answer} ({expected_start_pos}, {expected_end_pos})"
         # )
+
+        acceptable_answers = []
+        acceptable_answer_locs = []
+        for item in zip(labels.acceptable_answers, labels.acceptable_answers_starts, labels.acceptable_answers_ends):
+            # print('acceptable answer:', item)
+
+            acc_answer_start = item[1]
+            acc_answer_end = item[2] + 1
+
+            acc_answer_text = tokenizer.decode(input_id_list[acc_answer_start:acc_answer_end])
+            
+            acceptable_answers.append(acc_answer_text)
+            acceptable_answer_locs.append((acc_answer_start, acc_answer_end))
 
         return SimpleNamespace(
             answer=answer,
             answer_locs=(answer_start, answer_end),
             answer_probs = (answer_start_prob_max, answer_end_prob_max),
-            expected_answer=expected_answer,
-            expected_answer_locs=(expected_start_pos, expected_end_pos),
+            acceptable_answers=acceptable_answers,
+            acceptable_answer_locs=acceptable_answer_locs,
         )
 
     # for each batch in val data, run predictions and compute metrics
     num_items = 0
-    num_correct = 0
-    num_incorrect = 0
+    num_exact_correct = 0
+
+    total_normalized_levenshtein = 0
 
     for idx, batch in enumerate(dataloader):
         # print(f"processing batch: {batch}")
@@ -110,17 +135,6 @@ def cli(
         start_positions = batch["start_positions"].to(device)
         end_positions = batch["end_positions"].to(device)
 
-        print('n start:', start_positions.shape)
-
-        continue
-
-        # with torch.no_grad():
-        # outputs = model(
-        #     input_ids=batch["input_ids"],
-        #     attention_mask=batch["attention_mask"],
-        #     bbox=batch["bbox"],
-        #     pixel_values=batch["pixel_values"],
-        # )
         outputs = model(
             input_ids=input_ids,
             attention_mask=attention_mask,
@@ -133,35 +147,72 @@ def cli(
         # # log output
         # print(f"outputs: {outputs}")
 
-        labels = {
-            "start_positions": start_positions,
-            "end_positions": end_positions,
-        }
+        acceptable_answers = val_data[idx]["acceptable_answers"]
+        acceptable_answers_starts = val_data[idx]["acceptable_answers_starts"]
+        acceptable_answers_ends = val_data[idx]["acceptable_answers_ends"]
+
+        labels = SimpleNamespace(
+            acceptable_answers=acceptable_answers,
+            acceptable_answers_starts=acceptable_answers_starts,
+            acceptable_answers_ends=acceptable_answers_ends,
+        )
 
         pred_results = check_preds(outputs, labels)
-        # print(f"pred results: {pred_results}")
 
         # pretty print prediction info
-        print(f'expected: {pred_results.expected_answer}\t\t\t({pred_results.expected_answer_locs})')
-        print(f' predicted: {pred_results.answer}\t\t\t({pred_results.answer_locs})')
-        print(f'  answer probs: start {pred_results.answer_probs[0]}, end {pred_results.answer_probs[1]}')
+        print(f'expected: {pred_results.acceptable_answers}')
+        print(f' predicted: {pred_results.answer}')
+        print(f'  locs: {pred_results.answer_locs}')
+        print(f'  probs: {pred_results.answer_probs}')
 
-        is_correct = fuzzy(pred_results.answer, pred_results.expected_answer, 0.3)
-        print('  correct? ', is_correct)
+        if len(pred_results.acceptable_answers) == 0:
+            # no answer
+            continue
 
-        print()
+        # compute match validity
+        best_similarity = 0
+        best_answer_match = None
+
+        pred_answer = pred_results.answer.strip().lower()
+        
+        for acceptable_answer in pred_results.acceptable_answers:
+            acceptable_answer = acceptable_answer.strip().lower()
+            is_exact_match = acceptable_answer == pred_answer
+            if is_exact_match:
+                num_exact_correct += 1
+            
+            # check fuzzy matching
+            nl = normalized_levenshtein(acceptable_answer, pred_answer)
+            
+            # docvqa ANLS similarity metric
+            tau = 0.5
+            if nl <= tau:
+                similarity = 1 - nl
+            else:
+                # nl is too big
+                similarity = 0
+            
+            if similarity > best_similarity:
+                best_similarity = similarity
+                best_answer_match = acceptable_answer
+        
+        if best_answer_match:
+            print(' matched answer:', best_answer_match)
+        else:
+            print(' no match')
+        
+        total_normalized_levenshtein += best_similarity
+        
+        print(' best similarity:', best_similarity)
 
         num_items += 1
-        if is_correct:
-            num_correct += 1
-        else:
-            num_incorrect += 1
 
     # summarize results
     print(f"num items: {num_items}")
-    print(f"num correct: {num_correct}")
-    print(f"num incorrect: {num_incorrect}")
-    print(f"accuracy: {num_correct / num_items:.2%}")
+    print(f"num exact match: {num_exact_correct}")
+    print(f"accuracy: {num_exact_correct / num_items:.2%}")
+    anls = total_normalized_levenshtein / num_items
+    print(f"anls: {anls:.3f}")
 
 
 def main():
