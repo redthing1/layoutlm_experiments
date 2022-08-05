0a338397a73970b5be006201e824590df3b4a390
redthing1 <redthing1@alt.icu>
2022-08-04 23:33:24 -0700

    add model fuse script

---
+++ llm_tests/llm_tests/fuse_encdec.py
@@ -0,0 +1,59 @@
+import typer
+import os
+from typing import Optional, Dict, Tuple
+import collections
+import json
+import logging
+
+import torch
+import pandas as pd
+import numpy as np
+
+from transformers import  LayoutLMv3Model, AutoModelForCausalLM, EncoderDecoderModel
+from transformers import AutoProcessor, AutoModelForQuestionAnswering, AutoTokenizer
+
+from tqdm.auto import tqdm
+# from llm_tests.qa_trainer import QuestionAnsweringTrainer
+
+logger = logging.getLogger(__name__)
+
+def cli(
+    encoder_model_path: str,
+    decoder_model_path: str,
+    save_to: str = None,
+):
+    # # load the encoder model
+    # print(f"loading encoder model: {encoder_model_path}")
+    # model_encoder = LayoutLMv3Model.from_pretrained(encoder_model_path)
+    # processor = AutoProcessor.from_pretrained("microsoft/layoutlmv3-base", apply_ocr=True)
+    # tokenizer = AutoTokenizer.from_pretrained(encoder_model_path)
+    # print(f"encoder model loaded: {encoder_model_path}")
+
+    # # load the decoder model
+    # print(f"loading decoder model: {decoder_model_path}")
+    # model_decoder = AutoModelForCausalLM.from_pretrained(decoder_model_path)
+    # print(f"decoder model loaded: {decoder_model_path}")
+
+    # # fuse the models
+    # print("fusing models")
+    # seq2seq_model = EncoderDecoderModel(encoder=model_encoder, decoder=model_decoder)
+    # print("models fused")
+    # print(seq2seq_model)
+
+    # fuse the models
+    print(f"fusing models {encoder_model_path} and {decoder_model_path}")
+    seq2seq_model = EncoderDecoderModel.from_encoder_decoder_pretrained(encoder_model_path, decoder_model_path)
+    print("models fused")
+    print(seq2seq_model)
+
+    if save_to is not None:
+        print(f"saving model to {save_to}")
+        seq2seq_model.save_pretrained(save_to)
+        print(f"model saved to {save_to}")
+
+def main():
+    typer.run(cli)
+
+
+if __name__ == "__main__":
+    main()
